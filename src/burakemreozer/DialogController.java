8c0ba9b106ec5555615f7a3777a6f55dbf5462a2
Burak Emre Özer <burak.ozer@hacettepe.edu.tr>
2017-12-29 15:47:49 +0300

    Add files via upload

---
+++ src/burakemreozer/DialogController.java
@@ -0,0 +1,33 @@
+package burakemreozer;
+
+import burakemreozer.datamodel.TodoData;
+import burakemreozer.datamodel.TodoItem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
---
+++ src/burakemreozer/datamodel/TodoItem.java
@@ -0,0 +1,46 @@
+package burakemreozer.datamodel;
+
+import java.time.LocalDate;
+
+
+public class TodoItem {
+
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+//    @Override
+//    public String toString() {
+//        return shortDescription;
+//    }
+}
